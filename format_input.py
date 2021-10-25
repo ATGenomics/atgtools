417c52a3917ca6a37d7b62d7212c73128d942933
zorbax <otto94@gmail.com>
2021-10-25 11:22:57 +0200

    functools

--- format_input.py
+++ format_input.py
@@ -6,6 +6,7 @@ import re
 import numpy as np
 import json
 import pickle
+from functools import cmp_to_key
 
 
 def read_params():
@@ -125,34 +126,6 @@ def modify_feature_names(fn):
             ret2.append(r)
 
     return ret2
-
-
-def cmp_to_key(mycmp):
-    class K(object):
-        """Convert a cmp= function into a key= function"""
-
-        def __init__(self, obj, *args):
-            self.obj = obj
-
-        def __lt__(self, other):
-            return mycmp(self.obj, other.obj) < 0
-
-        def __gt__(self, other):
-            return mycmp(self.obj, other.obj) > 0
-
-        def __eq__(self, other):
-            return mycmp(self.obj, other.obj) == 0
-
-        def __le__(self, other):
-            return mycmp(self.obj, other.obj) <= 0
-
-        def __ge__(self, other):
-            return mycmp(self.obj, other.obj) >= 0
-
-        def __ne__(self, other):
-            return mycmp(self.obj, other.obj) != 0
-
-    return K
 
 
 def sort_by_cl(datals, n, c, s, u):
